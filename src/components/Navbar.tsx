fab7f912225033eb21d910acbd481fdaf4b07cdc
jakin <ijakinhy@gmail.com>
2025-07-11 14:10:52 +0200

    fix service route on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -137,7 +137,7 @@ export default function NavBar() {
                             className="text-white text-xl mt-3.5 py-4 border-b border-gray-800  transition-colors flex items-center justify-between"
                             onClick={toggleMenu}
                         >
-                            <TextSlideUpDown href="/" className="text-white  h-[24px]" label="SERVICES" />
+                            <TextSlideUpDown href="/services" className="text-white  h-[24px]" label="SERVICES" />
                             <ArrowUpRightIcon className="h-5 w-5 " />
                         </div>
                         <div
