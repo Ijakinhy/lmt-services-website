e506b1dbcf4ec3c56f96d84c0375d91131ab612d
jakin <ijakinhy@gmail.com>
2025-07-18 10:31:19 +0200

    fix resend endpoints

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -6,13 +6,18 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 
+
+
 export async function POST(req: Request) {
     const body = await req.json()
     const { firstName, lastName, companyName, visitorEmail, phoneNumber, message }: ContactFormData = body
+    if (!process.env.RESEND_API_KEY || !process.env.COMPANY_EMAIL) {
+        return NextResponse.json({ message: "Missing env variables" }, { status: 500 });
+    }
     try {
 
         await resend.emails.send({
-            from: 'Contact Form <onboarding@resend.dev>', // or your verified domain later
+            from: 'Contact Form <onboarding@resend.dev>',
             to: process.env.COMPANY_EMAIL!,
             subject: 'New Contact Form Submission',
             text: `
